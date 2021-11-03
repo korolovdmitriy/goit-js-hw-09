d04937eea228909db31148942fc01f0ca69ab53d
Dmitriy Korolov <adrorecords@gmail.com>
2021-11-03 16:38:07 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,43 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const form = document.querySelector('.form');
+
+form.addEventListener("submit", onSubmitClick);
+  
+function onSubmitClick (event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+   
+  let dalayPromise = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, dalayPromise).then(onFulfilled).catch(onRejected);;
+    dalayPromise += Number(step.value);
+  };
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+return new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (canFulfill) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }
+
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
+  }, delay);
+});
+  
+}
+
+function onFulfilled(result) {
+ Notify.success(result);
+}
+
+function onRejected(error) {
+ Notify.failure(error);
 }
 
 
-// Notify.failure('');
